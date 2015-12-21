9af75b857c4d06f7e429e37d22633ad2cfa14c9c
Rumsey <rums3918@cs-wormulon.cs.uidaho.edu>
2015-12-20 20:00:56 -0800

    update 210 num 1

---
+++ CS_210/hw6/uml2uni.java
@@ -0,0 +1,22 @@
+import java.io.*; 
+
+class uml2uni{
+    public static void main(String []args)
+    {
+
+
+
+
+    }
+
+    public void line_reader()
+    {
+	/*	try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+	       	String line;
+		while ((line = br.readLine()) != null) {
+		    // process the line.
+		}
+		}	*/ 
+    }
+
+}
